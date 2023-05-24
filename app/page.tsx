1ed523212ca63c6d378817231bca2b4670a9d3fd
ukorvl <ukorotovskiy@gmail.com>
2023-05-24 14:11:21 +0400

    minor chnages #1

--- app/page.tsx
+++ app/page.tsx
@@ -97,6 +97,14 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
+          <a
+            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            d
+          </a>
           <h2 className={'mb-3 text-2xl font-semibold'}>
             Deploy{' '}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
