f6f152f53d436463e50a040b463923bd31c703b4
ukorvl <ukorotovskiy@gmail.com>
2023-05-30 18:49:59 +0400

    add flowbite-react #1

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,8 @@
 import Image from 'next/image';
 
+/**
+ * @returns React component.
+ */
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  i18n: {
-    locales: ['en', 'am'],
-    defaultLocale: 'en',
-  },
+  
 };
 
 module.exports = nextConfig;
