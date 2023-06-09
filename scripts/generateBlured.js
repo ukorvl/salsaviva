8059154c8cec9f55a20d5d225bfe9fc5b3e63313
ukorvl <ukorotovskiy@gmail.com>
2023-06-09 04:13:50 +0400

    fix blur images script #4

--- scripts/generateBlured.js
+++ scripts/generateBlured.js
@@ -1,27 +1,69 @@
 const process = require('process');
 const fs = require('fs');
 const path = require('path');
+const jimp = require('jimp');
+const colors = require('colors/safe');
 
 const galleryDirName = 'public/gallery';
 const bluredDirName = 'blured';
 const pathGalleryDir = `${process.cwd()}/${galleryDirName}`;
-
-// Cd gallery images directory
-!fs.existsSync(pathGalleryDir) && fs.mkdirSync(pathGalleryDir);
-process.chdir(pathGalleryDir);
+const pathBluredDir = `${pathGalleryDir}/${bluredDirName}`;
 
 // Clear current blured images
-const pathBluredDir = `${process.cwd()}/${bluredDirName}`;
+!fs.existsSync(pathGalleryDir) && fs.mkdirSync(pathGalleryDir);
 !fs.existsSync(pathBluredDir) && fs.mkdirSync(pathBluredDir);
+process.chdir(pathGalleryDir);
 
-fs.readdir(pathBluredDir, (err, files) => {
-  if (err) throw err;
+const contents = getDirectoryContentFileNames(pathBluredDir);
+for (const c of contents) {
+  fs.unlink(path.join(pathBluredDir, c), (err) => {
+    if (err) throw err;
+  });
+}
+// eslint-disable-next-line no-console
+console.log(colors.green(`Cleaned up ${bluredDirName} directory`));
 
-  for (const file of files) {
-    fs.unlink(path.join(pathBluredDir, file), (err) => {
-      if (err) throw err;
+// Process gallery images and save into blured directory
+const images = getDirectoryContentFileNames(pathGalleryDir);
+// eslint-disable-next-line no-console
+console.log(colors.gray('\nStarting process images...\n'));
+for (const i of images) {
+  processImage(i);
+}
+
+// eslint-disable-next-line jsdoc/require-jsdoc
+function getDirectoryContentFileNames(dir) {
+  let fileNamesList = [];
+
+  fs.readdirSync(dir).forEach((file) => {
+    const fullPath = path.join(dir, file);
+
+    if (!fs.lstatSync(fullPath).isDirectory()) {
+      fileNamesList.push(file);
+    }
+  });
+
+  return fileNamesList;
+}
+
+// eslint-disable-next-line jsdoc/require-jsdoc
+function processImage(fileName) {
+  const pathToBlured = `${bluredDirName}/${fileName}`;
+
+  jimp.read(fileName)
+    .then((result) => {
+      return result
+        .quality(40)
+        .blur(30)
+        .rotate(90)
+        .write(pathToBlured);
+    })
+    .catch((err) => {
+      // eslint-disable-next-line no-console
+      console.error(err);
+    })
+    .then(() => {
+      // eslint-disable-next-line no-console
+      console.log(colors.green('Created blured image: ') + pathToBlured);
     });
-  }
-});
-
-console.log(process.cwd());
+}
