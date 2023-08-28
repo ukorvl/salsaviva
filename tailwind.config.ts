7af9f70c28f25a54c5b769d15330319853b81b09
ukorvl <ukorotovskiy@gmail.com>
2023-08-28 14:45:30 +0400

    add tailwind.config.ts #62

---
+++ tailwind.config.ts
@@ -0,0 +1,11 @@
+import type {Config} from 'tailwindcss';
+import theme from './tailwind/theme';
+
+export default {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme,
+} satisfies Config;
