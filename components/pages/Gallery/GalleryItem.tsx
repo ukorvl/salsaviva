5c8960dad3376df30384f7316eb9131ef58eae14
ukorvl <ukorotovskiy@gmail.com>
2023-09-02 23:46:27 +0400

    fixes #40 photoswipe gallery

--- components/pages/Gallery/GalleryItem.tsx
+++ components/pages/Gallery/GalleryItem.tsx
@@ -2,33 +2,23 @@ import {MutableRefObject} from 'react';
 import {Item} from 'react-photoswipe-gallery';
 import 'photoswipe/dist/photoswipe.css';
 import GalleryImage from './GalleryImage';
+import {GalleryConfigItem} from './config';
 
 /**
  * Props.
  */
-type GalleryItemProps = {
-  src: string;
-  blurDataURL: string;
-  dimensions: {
-    width: number;
-    height: number;
-  };
-};
+type GalleryItemProps = GalleryConfigItem;
 
 /**
  * @param {GalleryItemProps} props Props.
  * @returns React component.
  */
-export default function GalleryItem({
-  src,
-  blurDataURL,
-  dimensions: {width, height},
-}: GalleryItemProps) {
+export default function GalleryItem({src, blurDataURL, width, height, alt}: GalleryItemProps) {
   return (
     <Item
       original={src}
       thumbnail={src}
-      alt=""
+      alt={alt}
       width={width}
       height={height}
       key={src}
--- components/pages/Gallery/config.ts
+++ components/pages/Gallery/config.ts
@@ -1,27 +1,36 @@
+// eslint-disable-next-line jsdoc/require-jsdoc
+export type GalleryConfigItem = {
+  src: string;
+  blurDataURL: string;
+  width: number;
+  height: number;
+  alt?: string;
+};
+
 /**
  * Gallery images configuration.
  */
 const config = [
-  {fileName: 'gallery.0.jpg', dimensions: {width: 920, height: 1200}},
-  {fileName: 'gallery.1.jpg', dimensions: {width: 920, height: 600}},
-  {fileName: 'gallery.2.jpg', dimensions: {width: 920, height: 600}},
-  {fileName: 'gallery.3.jpg', dimensions: {width: 920, height: 600}},
-  {fileName: 'gallery.4.jpg', dimensions: {width: 920, height: 600}},
-  {fileName: 'gallery.5.jpg', dimensions: {width: 920, height: 600}},
-  {fileName: 'gallery.6.jpg', dimensions: {width: 920, height: 600}},
-  {fileName: 'gallery.7.jpg', dimensions: {width: 920, height: 600}},
-  {fileName: 'gallery.8.jpg', dimensions: {width: 920, height: 600}},
-  {fileName: 'gallery.9.jpg', dimensions: {width: 920, height: 600}},
-  {fileName: 'gallery.10.jpg', dimensions: {width: 920, height: 600}},
-  {fileName: 'gallery.11.jpg', dimensions: {width: 920, height: 600}},
-  {fileName: 'gallery.12.jpg', dimensions: {width: 920, height: 600}},
-  {fileName: 'gallery.13.jpg', dimensions: {width: 920, height: 600}},
-  {fileName: 'gallery.14.jpg', dimensions: {width: 920, height: 600}},
-  {fileName: 'gallery.15.jpg', dimensions: {width: 920, height: 600}},
+  {fileName: 'gallery.0.jpg', width: 640, height: 425, alt: ''},
+  {fileName: 'gallery.1.jpg', width: 920, height: 600, alt: ''},
+  {fileName: 'gallery.2.jpg', width: 920, height: 600, alt: ''},
+  {fileName: 'gallery.3.jpg', width: 920, height: 600, alt: ''},
+  {fileName: 'gallery.4.jpg', width: 920, height: 600, alt: ''},
+  {fileName: 'gallery.5.jpg', width: 920, height: 600, alt: ''},
+  {fileName: 'gallery.6.jpg', width: 920, height: 600, alt: ''},
+  {fileName: 'gallery.7.jpg', width: 920, height: 600, alt: ''},
+  {fileName: 'gallery.8.jpg', width: 920, height: 600, alt: ''},
+  {fileName: 'gallery.9.jpg', width: 920, height: 600, alt: ''},
+  {fileName: 'gallery.10.jpg', width: 920, height: 600, alt: ''},
+  {fileName: 'gallery.11.jpg', width: 920, height: 600, alt: ''},
+  {fileName: 'gallery.12.jpg', width: 920, height: 600, alt: ''},
+  {fileName: 'gallery.13.jpg', width: 920, height: 600, alt: ''},
+  {fileName: 'gallery.14.jpg', width: 920, height: 600, alt: ''},
+  {fileName: 'gallery.15.jpg', width: 1280, height: 854, alt: ''},
 ] as const;
 
-export default config.map(({fileName, dimensions}) => ({
-  dimensions,
+export default config.map<GalleryConfigItem>(({fileName, ...rest}) => ({
+  ...rest,
   src: `/images/${fileName}`,
   blurDataURL: `/images/blured/${fileName}`,
 }));
