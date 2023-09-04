f4b6295b6b9a6121c680d1fe45ada8985066b5ee
ukorvl <ukorotovskiy@gmail.com>
2023-09-04 12:13:54 +0400

    add form validation schema #8

---
+++ components/pages/Contact/Form/validationSchema.ts
@@ -0,0 +1,26 @@
+import {z} from 'zod';
+
+const requiredFields = z.object({
+  name: z.string().min(1, 'Name is required').max(50, 'Too Long!'),
+  message: z.string().max(500, 'Too Long!'),
+});
+
+const optionalFields = z
+  .object({
+    email: z.string().email('Invalid email address').max(50, 'Too Long!'),
+    telegram: z.string().startsWith('@', 'Invalid telegram nickname').max(50, 'Too Long!'),
+  })
+  .partial()
+  .refine(({email, telegram}) => email || telegram, {
+    message: 'Either email or telegram is required',
+    path: ['email', 'telegram'],
+  });
+
+const validationSchema = requiredFields.and(optionalFields);
+
+/**
+ * Type for the form data.
+ */
+export type ContactFormData = z.infer<typeof validationSchema>;
+
+export default validationSchema;
